036b24b126c0a1afddd5dd52c90797da717cb8c8
mmirandam205 <mariomiranda1986@icloud.com>
2025-07-03 19:58:34 -0600

    Update EC2.tf

--- EC2.tf
+++ EC2.tf
@@ -1,4 +1,4 @@
-variable "instance_name" {
+variable "INSTANCE_NAME" {
   description = "Nombre de la instancia EC2"
   type        = string
 }
@@ -7,10 +7,11 @@ resource "aws_instance" "mario_ec2" {
   ami           = "ami-0c803b171269e2d72"
   instance_type = "t3.micro"
   key_name      = "mariolab"
-  subnet_id     = "subnet-07264b405ef600089"
+
+  subnet_id              = "subnet-07264b405ef600089"
   vpc_security_group_ids = ["sg-00a0602acd5314d99"]
 
   tags = {
-    Name = var.instance_name
+    Name = var.INSTANCE_NAME
   }
 }
